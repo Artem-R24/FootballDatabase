0d094fae5408e84be27c5da1fb387e50f94dcd37
Artem-R24 <68281843+Artem-R24@users.noreply.github.com>
2021-11-02 16:09:54 +0300

    Add files via upload

---
+++ DBLab/Coach.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLab
+{
+    public class Coach
+    {
+        [Key]
+        [ForeignKey("Team")]
+        public int CoachId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public Team Team { get; set; }
+    }
+}
